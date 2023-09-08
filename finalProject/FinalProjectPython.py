da02c6446d5463a3f5209de9c0e5efd2bd659f79
aokll <pastuhov_1992@mail.ru>
2023-09-08 18:13:37 +0300

    version 4.0

--- finalProject/FinalProjectPython.py
+++ finalProject/FinalProjectPython.py
@@ -51,3 +51,45 @@ def edit_note():
     else: print('Заметка ненайдена')
 
 def delete_note():
+    id = input('Введите идентификатор заметки для удаления: ')
+    note = next((note for note in notes if note.id == id), None )
+    if note:
+        notes.remove(note)
+        save_notes(notes)
+    else: 
+        print('Заметка не найдена')
+
+def view_notes():
+    date_str = input('Введите дату для фильтрации заметок в формате dd.mm.yyyy: ')
+    try:
+        filter_date = datetime.strptime(date_str, '%d.%m.%y')
+        filtered_notes = [note for note in notes if datetime.strptime(note.date, '%d.%m.%Y %H:%M:%S').date() == filter_date.date()] 
+    except ValueError:
+        filtered_notes = notes
+    if filtered_notes:
+        for note in filtered_notes:
+            print(f'{note.id}{note.title}({note.date}):n {note.body}')
+        else
+            print('Нет заметок для отображения')
+
+def main():
+    global notes
+    notes = read_notes()
+
+    while True:
+        print('n 1. показать список заметокn 2. Добавить заметкуn 3. Редактировать заметкуn 4. Удалить заметкуn 5. Выходn')
+        choise = print('Выберите действие: ')
+        if choise == '1':
+            view_notes()
+        elif choise == '2':
+            add_note()
+        elif choise == '3':
+            edit_note()
+        elif choise == '4':
+            delete_note()
+        elif choise == '5';
+            break
+        else
+            print('Недопустимый выбор')                 
+
+                
